02d2b22472eb8785423979c1b5d9356d5665a170
mr-atuzie <rexatuzie@gmail.com>
2024-01-06 01:55:46 +0100

    proctect Middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,13 +1,33 @@
 const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
+const User = require("../models/user");
 
-const protect = asyncHandler((req, res) => {
+const protect = asyncHandler(async (req, res, next) => {
   try {
     const token = res.cookies.token;
+
     if (!token) {
       res.status(401);
       throw new Error("Not authorized, please login");
     }
+
+    //Verify token
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!verified) {
+      res.status(401);
+      throw new Error("Token has expired, please login");
+    }
+
+    const user = await User.findById(verified.id).select("-password");
+
+    if (!user) {
+      res.status(401);
+      throw new Error("User not found");
+    }
+
+    req.user = user;
+    next();
   } catch (error) {
     res.status(401);
     throw new Error("Not authorized, please login");
